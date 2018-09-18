d6e0b214325d5d9d2d8a7ffe32a5d38b78654ccd
portfolio123 <43347146+portfolio123@users.noreply.github.com>
2018-09-18 17:09:08 +0300

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+               $("#link1").click(function(){
+                    $path=$("#portfolio").offset().top;
+                    $('body').animate({scrollTop:$path},1000);
+               });
+			   $("#link2").click(function(){
+                    $path=$("#about").offset().top;
+                    $('body').animate({scrollTop:$path},1000);
+               });
+               $("#link3").click(function(){
+                    $path=$("#contact").offset().top;
+                    $('body').animate({scrollTop:$path},1000);
+               });
+            });
+			
+			
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){
+$('#header').addClass("sticky");
+}
+else{
+$('#header').removeClass("sticky");
+}
+});
