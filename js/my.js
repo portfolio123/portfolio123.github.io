922bbd58dbebfacac8b9a3be3e4aaa0926aecd52
portfolio123 <43347146+portfolio123@users.noreply.github.com>
2020-05-13 12:13:37 +0300

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function(){
+			// Фикмированная шапка при скролле
+			$("#header").removeClass("default");
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 20) {
+					$("#header").addClass("default").fadeIn('fast');
+				} else {
+					$("#header").removeClass("default").fadeIn('fast');
+				};
+			});
+
+			// Плавный скролл по якорям
+			//Работающий при любых jquery
+			$('a[href^="#"]').bind("click", function(e){
+			  var anchor = $(this);
+			  $('html, body').stop().animate({
+			   scrollTop: $(anchor.attr("href")).offset().top
+			  }, 1000);
+			  e.preventDefault();
+			 });
+			 return false;﻿
+		});
+
+		
+/* Burger*/
+
+(function(){
+    const burgerItem=document.querySelector('.burger');
+    const menu=document.querySelector('.header_nav');
+    const menuCloseItem=document.querySelector('.header_nav-close');
+    burgerItem.addEventListener('click',()=>{
+        menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click',()=>{
+        menu.classList.remove('header_nav_active');
+    });
+}());
