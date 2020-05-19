0d1f370dc262b194039312208006ee02089ba0d7
portfolio123 <43347146+portfolio123@users.noreply.github.com>
2020-05-19 20:39:58 +0300

    Add files via upload

---
+++ Gray_product/js/my10.js
@@ -0,0 +1,18 @@
+﻿$('#play-video').on('click', function(e){
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/vIEtEnr_5m0?version=3&loop=1&autoplay=1&modestbranding=0&showinfo=0&rel=0&end=78" frameborder="0" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode === 27) { close_video().click; }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
