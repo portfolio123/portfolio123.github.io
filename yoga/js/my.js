98f75d359017fb2cdb4bc0c4556d52f434791b90
portfolio123 <43347146+portfolio123@users.noreply.github.com>
2020-07-27 20:00:27 +0300

    Add files via upload

---
+++ yoga/js/my.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function(){
+
+	$('.slider').on('init', function(event, slick){
+  console.log('.slider init');
+
+        AOS.init({
+            easing: 'ease-out-back',
+            duration: 1000
+        });
+	});
+	
+	$('.slider').slick({
+		
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		slidesToScroll:1,
+		speed:1000,
+		infinite: true,
+		autoplay: false,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},{
+				breakpoint: 480,
+				settings: {
+					slidesToShow:1
+				}
+			},{
+				breakpoint: 320,
+				settings: {
+					slidesToShow:1
+				}
+			}
+			
+		]
+	});
+});
