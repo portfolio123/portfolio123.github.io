9cf44efafd7e82abde4c39cd2f1f6a0a60282f5d
portfolio123 <43347146+portfolio123@users.noreply.github.com>
2020-07-27 19:58:38 +0300

    Add files via upload

---
+++ library/main.js
@@ -0,0 +1,13 @@
+﻿/* Burger*/
+
+(function(){
+    const burgerItem=document.querySelector('.burger');
+    const menu=document.querySelector('.right_nav');
+    const menuCloseItem=document.querySelector('.header_nav-close');
+    burgerItem.addEventListener('click',()=>{
+        menu.classList.add('right_nav_active');
+    });
+    menuCloseItem.addEventListener('click',()=>{
+        menu.classList.remove('right_nav_active');
+    });
+}());
